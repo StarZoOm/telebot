a1cc2839c888730df512249a870c21751a47ed4c
StarZoOm <kulevkir@gmail.com>
2019-11-22 10:24:50 +0300

    my first telebot

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 from Bot.config import TOKEN
 import telebot
 from telebot import apihelper
+
 apihelper.proxy = {'https': 'socks5://defaultproxyuser2:nwqCxPM6TCEXG8d@socks5.momentum.rest:1080'}
 
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'This is test')
 
-bot.polling()
+
+if __name__ == '__main__':
+    bot.polling()
