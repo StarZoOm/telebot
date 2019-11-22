e178c929ba0506c46391ed999d7623b1e02c77d7
StarZoOm <kulevkir@gmail.com>
2019-11-22 10:20:38 +0300

    my first telebot

--- bot.py
+++ bot.py
@@ -7,6 +7,6 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'ПОДЪЁЁЁЁЁЁМ!')
+    bot.send_message(message.chat.id, 'This is test')
 
 bot.polling()
