70686347e2f3f158ef83780afa23f11bd6046aa4
StarZoOm <kulevkir@gmail.com>
2019-11-22 10:59:01 +0300

    my first telebot

--- bot.py
+++ bot.py
@@ -7,9 +7,20 @@ apihelper.proxy = {'https': 'socks5://defaultproxyuser2:nwqCxPM6TCEXG8d@socks5.m
 bot = telebot.TeleBot(TOKEN)
 
 
+bot.send_message(480303845, 'i`m fine...')
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'This is test')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Твой id = ' + str(message.chat.id))
+        print(message.chat.id)
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
 
 
 if __name__ == '__main__':
